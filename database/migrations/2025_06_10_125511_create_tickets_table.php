0fd0ffdf268d86d5f793d430f5075df39d54e6ae
basel-kaffoura <baselkaffoura@gmail.com>
2025-06-11 16:40:50 +0400

    Simple code enhancement

--- database/migrations/2025_06_10_125511_create_tickets_table.php
+++ database/migrations/2025_06_10_125511_create_tickets_table.php
@@ -6,19 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    protected array $connections = ['technical', 'billing', 'product', 'general', 'feedback'];
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        $connections = ['technical', 'billing', 'product', 'general', 'feedback'];
-
-        foreach ($connections as $connection) {
+        foreach ($this->connections as $connection) {
             Schema::connection($connection)->create('tickets', function (Blueprint $table) {
                 $table->id();
                 // Use a unique ticket_number because the auto increment id may be duplicated across databases
                 $table->string('ticket_number')->unique();
-                $table->enum('ticket_type', ['technical', 'billing', 'product', 'general', 'feedback']);
+                $table->enum('ticket_type', $this->connections);
                 $table->string('name');
                 $table->string('email');
                 $table->string('subject');
@@ -34,9 +33,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        $connections = ['technical', 'billing', 'product', 'general', 'feedback'];
-
-        foreach ($connections as $connection) {
+        foreach ($this->connections as $connection) {
             Schema::connection($connection)->dropIfExists('tickets');
         }
     }
